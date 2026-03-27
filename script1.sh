d1ebb6e30bdd72a8ab4dbe451a4de8cdab26bf76
Mridul611 <haritripathi415@gmail.com>
2026-03-27 20:13:50 +0530

    Add system identity report script

---
+++ script1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Mridul Hari Tripathi | Course: Open Source Software
+
+# --- Variables ---
+STUDENT_NAME="Mridul Hari Tripathi"
+SOFTWARE_CHOICE="Python"
+
+# --- System info ---
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+HOME_DIR=$HOME
+DATE=$(date)
+DISTRO=$(lsb_release -d | cut -f2)
+PYTHON_VERSION=$(python3 --version)
+
+# --- Display ---
+echo " Open Source Audit — $STUDENT_NAME"
+echo "Software      : $SOFTWARE_CHOICE"
+echo "Distribution  : $DISTRO"
+echo "Kernel        : $KERNEL"
+echo "User          : $USER_NAME"
+echo "Home Directory: $HOME_DIR"
+echo "Uptime        : $UPTIME"
+echo "Date          : $DATE"
